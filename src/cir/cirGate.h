43ffa0d6f89935682e912e031b1a4306916a75e9
gillbertS <ldes89150@gmail.com>
2014-12-31 14:26:53 +0800

    initial commit

---
+++ src/cir/cirGate.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+  FileName     [ cirGate.h ]
+  PackageName  [ cir ]
+  Synopsis     [ Define basic gate data structures ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2008-2014 LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#ifndef CIR_GATE_H
+#define CIR_GATE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "cirDef.h"
+#include "sat.h"
+
+using namespace std;
+
+// TODO: Feel free to define your own classes, variables, or functions.
+
+class CirGate;
+
+//------------------------------------------------------------------------
+//   Define classes
+//------------------------------------------------------------------------
+class CirGate
+{
+public:
+   CirGate() {}
+   virtual ~CirGate() {}
+
+   // Basic access methods
+   string getTypeStr() const { return ""; }
+   unsigned getLineNo() const { return 0; }
+
+   // Printing functions
+   virtual void printGate() const = 0;
+   void reportGate() const;
+   void reportFanin(int level) const;
+   void reportFanout(int level) const;
+
+private:
+
+protected:
+};
+
+#endif // CIR_GATE_H
